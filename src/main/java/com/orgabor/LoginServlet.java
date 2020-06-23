1bfb85f047c74e5f436433529ceb7dd31654c5a9
Gabor Orban <orbangabeszb@gmail.com>
2020-06-23 18:08:32 +0200

    doGet method added with PrintWriter

--- src/main/java/com/orgabor/LoginServlet.java
+++ src/main/java/com/orgabor/LoginServlet.java
@@ -1,10 +1,20 @@
 package com.orgabor;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = "/login.do")
 public class LoginServlet extends HttpServlet {
 	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		PrintWriter writer = res.getWriter();
+	}
 
 }
