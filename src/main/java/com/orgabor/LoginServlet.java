20ece3198a915c9bbdaf090d2c0f492f8314240b
Gabor Orban <orbangabeszb@gmail.com>
2020-06-23 18:05:29 +0200

    LoginServlet class started from scratch

---
+++ src/main/java/com/orgabor/LoginServlet.java
@@ -0,0 +1,10 @@
+package com.orgabor;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/login.do")
+public class LoginServlet extends HttpServlet {
+	
+
+}
