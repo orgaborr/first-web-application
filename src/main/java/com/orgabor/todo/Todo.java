c67d2543b84c953ec0795c8221ef47fc2482b34d
Gabor Orban <orbangabeszb@gmail.com>
2020-07-02 17:34:58 +0200

    category field and related methods added to Todo class

--- src/main/java/com/orgabor/todo/Todo.java
+++ src/main/java/com/orgabor/todo/Todo.java
@@ -2,9 +2,11 @@ package com.orgabor.todo;
 
 public class Todo {
 	private String name;
+	private String category;
 
-	public Todo(String name) {
+	public Todo(String name, String category) {
 		this.name = name;
+		this.category = category;
 	}
 
 	public String getName() {
@@ -14,10 +16,18 @@ public class Todo {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
 
 	@Override
 	public String toString() {
-		return String.format("Todo [name=%s]", name);
+		return String.format("Todo [name=%s, category=%s]", name, category);
 	}
 
 	@Override
