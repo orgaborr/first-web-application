182261430dc21f0cd0deea289a543e29b6bfd07a
Gabor Orban <orbangabeszb@gmail.com>
2020-07-02 16:33:08 +0200

    logout added to todo.jsp

--- src/main/java/com/orgabor/logout/LogoutServlet.java
+++ src/main/java/com/orgabor/logout/LogoutServlet.java
@@ -15,6 +15,7 @@ public class LogoutServlet extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		req.getSession().invalidate();
 		req.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(req, res);
 	}
 }
